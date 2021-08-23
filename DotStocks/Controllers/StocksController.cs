30bcc2ba57e4c497e209b067f32ab324e353e0b3
Lars Skyum <lrskyum@gmail.com>
2021-08-24 00:01:20 +0200

    Get alphavantage response

--- DotStocks/Controllers/StocksController.cs
+++ DotStocks/Controllers/StocksController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace DotStocks.Controllers
 {
@@ -22,21 +25,41 @@ namespace DotStocks.Controllers
 
         public double Adjusted => adjusted;
     }
-    
+
+    class AlphaVantageResponseDto
+    {
+    }
+
     [ApiController]
     [Route("[controller]")]
     public class StocksController : ControllerBase
     {
         private const String alphaVantageKey = "EJSFNVGS7Q00C4N6";
-        
-        private String alphaVantageTemplate =
-            "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${key}";
+
+        private Uri getAlphaVantageUrl(String symbol) =>
+            new Uri(
+                $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={alphaVantageKey}");
 
         [HttpGet]
-        public async IEnumerable<List<Quote>> getQuotes(String symbol)
+        public Task<String> getQuotes(String symbol)
         {
-            var httpClient = new HttpClient();
-            await httpClient.GetAsync()
+            String res = null;
+            var url = getAlphaVantageUrl(symbol);
+            var httClient = new HttpClient();
+            Task<HttpResponseMessage> trm = httClient.GetAsync(url);
+            
+            trm.ContinueWith(responseTask =>
+            {
+                var response = responseTask.Result;
+
+                // return response.Content.ReadAsStringAsync().ContinueWith(jsonTask =>
+                // {
+                //     var json = jsonTask.Result;
+                //     String result = JsonConvert.DeserializeObject<String>(json);
+                //     return result;
+                // });
+            });
+            return null;
         }
     }
 }
--- DotStocksTest/UnitTest1.cs
+++ DotStocksTest/UnitTest1.cs
@@ -1,20 +1,25 @@
+using System.Threading;
 using DotStocks.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 
 namespace NUnitTests
 {
     public class Tests
     {
+        private StocksController _controller;
+
         [SetUp]
         public void Setup()
         {
-            StocksController controller = new StocksController();
-            
+            _controller = new StocksController();
         }
 
         [Test]
         public void Test1()
         {
+            _controller.getQuotes("IBM");
+            Thread.Sleep(200000);
             Assert.Pass();
         }
     }
