40096d8ac1579e714b3317724fa88208440a1fc1
LRS <lars.ruben.skyum@systematic.com>
2021-08-24 09:32:49 +0200

    Removed bins

--- DotStocks/Controllers/StocksController.cs
+++ DotStocks/Controllers/StocksController.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace DotStocks.Controllers
 {
@@ -25,41 +22,21 @@ namespace DotStocks.Controllers
 
         public double Adjusted => adjusted;
     }
-
-    class AlphaVantageResponseDto
-    {
-    }
-
+    
     [ApiController]
     [Route("[controller]")]
     public class StocksController : ControllerBase
     {
         private const String alphaVantageKey = "EJSFNVGS7Q00C4N6";
+        
+        private String alphaVantageTemplate =
+            "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${key}";
 
-        private Uri getAlphaVantageUrl(String symbol) =>
-            new Uri(
-                $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={alphaVantageKey}");
-
-        [HttpGet]
-        public Task<String> getQuotes(String symbol)
-        {
-            String res = null;
-            var url = getAlphaVantageUrl(symbol);
-            var httClient = new HttpClient();
-            Task<HttpResponseMessage> trm = httClient.GetAsync(url);
-            
-            trm.ContinueWith(responseTask =>
-            {
-                var response = responseTask.Result;
-
-                // return response.Content.ReadAsStringAsync().ContinueWith(jsonTask =>
-                // {
-                //     var json = jsonTask.Result;
-                //     String result = JsonConvert.DeserializeObject<String>(json);
-                //     return result;
-                // });
-            });
-            return null;
-        }
+        // [HttpGet]
+        // public async IEnumerable<List<Quote>> getQuotes(String symbol)
+        // {
+        //     var httpClient = new HttpClient();
+        //     await httpClient.GetAsync()
+        // }
     }
 }
